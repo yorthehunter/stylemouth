1b993dd8a0077135588ba0c02e054228a6df2aa1
yorthehunter <boru.design@gmail.com>
2014-02-16 18:03:13 -0500

    new app spun up with SASS and Haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Litemouth::Application.config.secret_token = '78770004d1e8a89ab3a51bb596a699cede39fddf9cbc591556741114c7ea9a5306bf8b82ce49d62f23d5bde82d375b475e7f57e5baf56bf05cac6edababf589d'
